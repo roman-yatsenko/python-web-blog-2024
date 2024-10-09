60968e94cc6aab28493b8846721db54dd60a7009
YaRo <roman.yatsenko@hneu.net>
2024-10-09 18:02:16 +0300

    Add 05.10.24

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 
@@ -42,4 +43,7 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
     
