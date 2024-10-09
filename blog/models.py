8be8f1257b99d40d2c74eab0f180f1dc405f0057
YaRo <roman.yatsenko@hneu.net>
2024-10-09 18:33:44 +0300

    09.10.24 18:33:43

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
 from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
 
@@ -45,5 +45,13 @@ class Post(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
+        return reverse(
+            "blog:post_detail", 
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
     
--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,9 @@ app_name = 'blog'
 urlpatterns = [
     # види постів блогу
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path(
+        '<int:year>/<int:month>/<int:day>/<slug:post>/', 
+        views.post_detail, 
+        name='post_detail'
+    ),
 ]
