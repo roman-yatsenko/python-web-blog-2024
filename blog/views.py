04805bf45830105303269dcc35c1c59d73448355
YaRo <roman.yatsenko@hneu.net>
2024-10-09 20:05:20 +0300

    Add 09.10.24 20:05:18

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,30 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 # Create your views here.
 
+class PostListView(ListView):
+    """Альтернативне подання списку постів"""
+    queryset=Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Посторінкове розбиття з 3 постами на сторінку
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(
         request,
         'blog/post/list.html',
@@ -27,3 +46,17 @@ def post_detail(request, year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
     )
+
+def post_share(request, post_id):
+    # Отримати пост за id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        # Форма передана на обробку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля форми успішно пройшли валідацію
+            cd = form.cleaned_data
+            # ... відправити електронного листа
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
