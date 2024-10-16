63736b178d0970c45bb3dae4054dab41e3651086
ragnirim <ragnirim@gmail.com>
2024-10-16 20:01:18 +0300

    Add comment form

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Comment, Post
 
 # Register your models here.
 
@@ -13,4 +13,11 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
-    
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
+        
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
-from .models import Post
+from .forms import CommentForm, EmailPostForm
+from .models import Comment, Post
 
 # Create your views here.
 
@@ -68,3 +69,18 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # Коментар було відправлено
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Створити об'єкт Comment, не зберігаючи його в БД
+        comment = form.save(commit=False)
+        # Призначити пост коментарю
+        comment.post = post
+        # Зберегти коментар в БД
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
