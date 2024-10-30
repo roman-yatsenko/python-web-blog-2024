bb9b32c1edf66f04b9211b858346e26c71841010
roman-yatsenko <roman.yatsenko@hneu.net>
2024-10-30 19:58:40 +0200

    Add sitemap

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -36,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'blog',
     'crispy_forms',
